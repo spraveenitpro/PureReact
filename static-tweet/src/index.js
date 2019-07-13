a3dcc4ea75cd707cdea665502da21f7c1d70628d
praveen <spraveenitpro@gmail.com>
2019-07-12 23:41:09 -0700

    Modelling a Tweet Component with React

---
+++ static-tweet/src/index.js
@@ -0,0 +1,56 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+function Tweet() {
+	return (
+		<div className='tweet'>
+			<Avatar />
+			<div className='content'>
+				<NameWithHandle />
+				<Message />
+				<div className='buttons'>
+					<ReplyButton />
+					<RetweetButton />
+					<LikeButton />
+					<MoreOptionsButton />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function Avatar() {
+	return (
+		<img
+			src='https://www.gravatar.com/avatar/80ad7e627570e773e0048ffb5208bba3'
+			className='avatar'
+			alt='avatar'
+		/>
+	);
+}
+
+function Message() {
+	return <div className='message'>This should be less than 140 char</div>;
+}
+
+function NameWithHandle() {
+	return (
+		<span className='name-with-handle'>
+			<span className='name'>Your Name</span>
+			<span className='handle'>@yourhandle</span>
+		</span>
+	);
+}
+
+const Time = () => <span className='time'>3h ago</span>;
+
+const ReplyButton = () => <i className='fa fa-reply reply-button' />;
+
+const RetweetButton = () => <i className='fa fa-retweet retweet-button' />;
+
+const LikeButton = () => <i className='fa fa-heart like-button' />;
+
+const MoreOptionsButton = () => <i className='fa fa-ellipsis-h more-options-button' />;
+
+ReactDOM.render(<Tweet />, document.getElementById("root"));

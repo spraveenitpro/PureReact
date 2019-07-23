391ed19ca739d460b7224cea052bf3a12a4dee88
praveen <spraveenitpro@gmail.com>
2019-07-22 23:40:37 -0700

    Dialog example with children props

--- children-exercises/src/index.js
+++ children-exercises/src/index.js
@@ -2,80 +2,61 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// CHILDREN EXERCISE 1
-const ErrorBox = ({ children }) => {
+function Dialog({ children }) {
+	let title, body, footer;
+
+	React.Children.forEach(children, child => {
+		switch (child.type) {
+			case Title:
+				title = child;
+				break;
+			case Body:
+				body = child;
+				break;
+			case Footer:
+				footer = child;
+				break;
+			default:
+				throw new Error("Dialog can only contain Title, Body and Footer!!");
+		}
+	});
+
 	return (
-		<div className='alert-danger' id='warning'>
-			<i class='fas fa-exclamation-triangle fa-2x' />
-			{children}
+		<div className='modal show'>
+			<div className='modal-dialog'>
+				<div className='modal-content'>
+					<div className='modal-header'>{title}</div>
+					<div className='modal-body'>{body}</div>
+					<div className='modal-footer'>{footer}</div>
+				</div>
+			</div>
 		</div>
 	);
-};
+}
 
-// CHILDREN EXERCISE 2
+function Title({ children }) {
+	return <h4>{children}</h4>;
+}
 
-const FirstChildOnly = ({ children }) => {
-	let items = React.Children.toArray(children);
-	let firstItem = items.splice(0, 1);
-	return <div>{firstItem}</div>;
-};
+function Body({ children }) {
+	return <h4>{children}</h4>;
+}
 
-const LastChildOnly = ({ children }) => {
-	let items = React.Children.toArray(children);
-	let lastItem = items.splice(items.length - 1, 1);
-	return <div>{lastItem}</div>;
-};
+function Footer({ children }) {
+	return <h4>{children}</h4>;
+}
 
-const Head = ({ number, children }) => {
-	let items = React.Children.toArray(children);
-	let someItems = items.slice(0, number);
-	return <div>{someItems}</div>;
-};
-
-const Tail = ({ number, children }) => {
-	let items = React.Children.toArray(children);
-	let someItems = items.splice(items.length - number, items.length);
-	return <div>{someItems}</div>;
-};
-
-const App = () => {
+function Test() {
 	return (
-		<>
-			<ErrorBox>Testing!</ErrorBox>;
-			<FirstChildOnly>
-				<h1>This is first Child</h1>
-				<h2>This is the second Child</h2>
-				<h3>This is the 3rd Child</h3>
-			</FirstChildOnly>
-			<LastChildOnly>
-				<h1>This is first Child</h1>
-				<h2>This is the second Child</h2>
-				<h3>This is the 3rd Child</h3>
-			</LastChildOnly>
-			<Head number={3}>
-				<p>1</p>
-				<p>2</p>
-				<p>3</p>
-				<p>4</p>
-				<p>5</p>
-				<p>6</p>
-				<p>7</p>
-				<p>8</p>
-				<p>9</p>
-			</Head>{" "}
-			<Tail number={4}>
-				<p>1</p>
-				<p>2</p>
-				<p>3</p>
-				<p>4</p>
-				<p>5</p>
-				<p>6</p>
-				<p>7</p>
-				<p>8</p>
-				<p>9</p>
-			</Tail>
-		</>
+		<Dialog>
+			<Title>This is da Title, Biatches..</Title>
+			<Body>This is the body, if you like it...</Body>
+			<Footer>
+				<button className='btn btn-default'>
+					<em>Close!!!</em>
+				</button>
+			</Footer>
+		</Dialog>
 	);
-};
-
-ReactDOM.render(<App />, document.getElementById("root"));
+}
+ReactDOM.render(<Test />, document.querySelector("#root"));

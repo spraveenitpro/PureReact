96943e0deaff67d92392d4dd931dce4426f30bd6
praveen <spraveenitpro@gmail.com>
2019-08-01 23:26:31 -0700

    Refactor simple counter to play with state

--- state/src/index.js
+++ state/src/index.js
@@ -2,47 +2,50 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-// function handleAction(event) {
-// 	console.log("Child did:", event);
-// }
-
-// function Parent() {
-// 	return <Child onAction={handleAction} />;
-// }
-
 class CountingParent extends React.Component {
 	state = {
 		actionCount: 0
 	};
 
 	handleAction = action => {
-		console.log("child says: ", action);
 		this.setState({
 			actionCount: this.state.actionCount + 1
+		});
+	};
+
+	resetAction = action => {
+		this.setState({
+			actionCount: 0
 		});
 	};
 
 	render() {
 		return (
 			<div>
-				<Child onAction={this.handleAction} />
-				<p>Clicked {this.state.actionCount} times</p>
+				<Child onAction={this.handleAction} onReset={this.resetAction} />
+				<p>Clicked {this.state.actionCount} Times!</p>
 			</div>
 		);
 	}
 }
 
-function Child({ onAction }) {
-	return <button onClick={onAction}> Click Me!</button>;
+function Child({ onAction, onReset }) {
+	return (
+		<div>
+			<button onClick={onAction}>Click here!</button>
+			<button onClick={onReset}>Reset</button>
+		</div>
+	);
 }
 
-const Page = () => (
-	<div>
-		<CountingParent />
-		<CountingParent />
-		<CountingParent />
-	</div>
-);
+function Page() {
+	return (
+		<div>
+			<CountingParent />
+			<CountingParent />
+		</div>
+	);
+}
 
 ReactDOM.render(<Page />, document.getElementById("root"));
 

2ce153e7f8e939153cf63d247ed846ff0461f1a5
praveen <spraveenitpro@gmail.com>
2019-08-21 23:57:40 -0700

    Finished Item and Itempages for Shopper

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -1,16 +1,35 @@
 import React from "react";
 import Nav from "./Nav";
 import "./App.css";
+import ItemPage from "./ItemPage";
+import { items } from "./static-data";
 
 class App extends React.Component {
+	state = {
+		activeTab: 0
+	};
+
+	handleTabChange = index => {
+		this.setState({
+			activeTab: index
+		});
+	};
+
 	renderComponent() {
-		return <span>Empty</span>;
+		switch (this.state.activeTab) {
+			default:
+			case 0:
+				return <ItemPage items={items} />;
+			case 1:
+				return <span>Cart</span>;
+		}
 	}
 
 	render() {
+		let { activeTab } = this.state;
 		return (
 			<div className='App'>
-				<Nav />
+				<Nav activeTab={activeTab} onTabChange={this.handleTabChange} />
 				<main className='App-content'>{this.renderComponent()}</main>
 			</div>
 		);

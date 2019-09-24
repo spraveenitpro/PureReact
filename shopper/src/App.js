f0fe6a52284a4d62ac36dfded9ac5ba47ad7f46d
praveen <spraveenitpro@gmail.com>
2019-09-23 23:23:31 -0700

    starting Shopper from scratch

--- shopper/src/App.js
+++ shopper/src/App.js
@@ -1,47 +1,26 @@
-import React from "react";
-import Nav from "./Nav";
-import "./App.css";
-import ItemPage from "./ItemPage";
-import { items } from "./static-data";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends React.Component {
-	state = {
-		activeTab: 0,
-		cart: []
-	};
-
-	handleTabChange = index => {
-		this.setState({
-			activeTab: index
-		});
-	};
-
-	handleAddToCart = item => {
-		this.setState({
-			cart: [...this.state.cart, item.id]
-		});
-	};
-
-	renderContent() {
-		switch (this.state.activeTab) {
-			default:
-			case 0:
-				return <ItemPage items={items} onAddtoCart={this.handleAddToCart} />;
-			case 1:
-				return <span>Cart</span>;
-		}
-	}
-
-	render() {
-		let { activeTab } = this.state;
-		return (
-			<div className='App'>
-				<Nav activeTab={activeTab} onTabChange={this.handleTabChange} />
-				<main className='App-content'>{this.renderContent()}</main>
-				<div>{this.state.cart.length} Items</div>
-			</div>
-		);
-	}
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
